180cb986859042428c109832e8da34dad907cfbf
Fabrizio1919 <fabriziosalerno19@gmail.com>
2023-03-15 14:24:19 +0100

    Add structure html css js - Add list in js - Add list in html with ul and style in css

---
+++ assets/js/main.js
@@ -0,0 +1,15 @@
+/* Data una lista della spesa, stampare sulla pagina (anche brutalmente, basta che si vedano) gli elementi della lista individualmente con un ciclo while. */
+
+// list-items
+const listItems = ["Latte", "Acqua", "Pasta", "Coca cola", "Frutta", "Pane", "Uova"];
+const shoppingList = document.getElementById("shopping-list");
+
+let i = 0;
+
+// loop
+while (i < listItems.length) {
+    let item = `<li>${listItems[i]}</li>`
+    shoppingList.innerHTML += item;
+    i++;  
+}
+
